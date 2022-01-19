bed47360c223d26ba68f839b23f96631fb76f5dd
Muhammad Azzam <MazzamR08@gmail.com>
2022-01-19 11:06:53 +0700

    blok scope

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+// let nama1 = 'john doe' //pendeklarasian variable
+// let nama2 = 'johan'
+// console.log(nama) //statement
+// console.log(nama2)
+
+// expresion
+
+// expresion adalah code js untuk menghasilkan nilai tertentu
+
+// let a = 10
+// let b = 20
+// let c = a * b   //statement yg berisi expresion / menghasilkan nilai tertentu
+// console.log(c)
+
+// block scope 
+
+function hello(){
+    if(true){
+        let manggil = 'halo mate'
+        console.log(manggil)
+    }
+    console.log(manggil)
+}
+hello()
