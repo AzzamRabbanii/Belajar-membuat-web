2e439344589a149d4b8cf6ff25eaa34070c4b275
Muhammad Azzam <MazzamR08@gmail.com>
2022-01-26 15:08:29 +0700

    challange

---
+++ challange.js
@@ -0,0 +1,22 @@
+// nomor satu
+
+let tipe = 'contoh'
+let tipe2 = true
+let name
+let tipe3 = {}
+console.log('ini adalah tipe data ' + typeof tipe  + 
+'\nini adalah tipe data ' + typeof tipe2 + 
+'\nini adalah tipe data ' + typeof name +
+'\nini adalah tipe data ' + typeof tipe3)
+
+// nomor dua
+
+let contoh = ['hello', 'assalamualaikum', 'selamat datang', 'semangat!!']
+
+for (i = 0; i < contoh.length; i++){
+    console.log(contoh[i])
+    console.log(i)
+}
+
+// nomor tiga
+
